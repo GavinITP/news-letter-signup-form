24df10bff1eacb53168e50860d06fa9b62a8b924
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-05-30 16:15:39 +0700

    set up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "hsl(4, 100%, 67%)",
+      slate: "hsl(234, 29%, 20%)",
+      charcoal: "hsl(235, 18%, 26%)",
+      grey: "hsl(231, 7%, 60%)",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-roboto)"],
       },
     },
   },
-  plugins: [],
-}
+};
