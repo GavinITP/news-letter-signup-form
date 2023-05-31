86f6fa52afd4b27ab14764ac4a168b8da47e0f99
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-05-31 20:17:47 +0700

    responsive image

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,20 @@ const Home = () => {
   };
 
   return (
-    <main className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full sm:max-w-[375px] md:max-w-[1000px] 2xl:scale-125 shadow-2xl">
-      <article className="bg-white sm:rounded-2xl md:rounded-3xl overflow-hidden min-h-screen sm:min-h-min grid md:grid-cols-2 md:p-8 md:gap-8">
-        <img
-          className="w-full md:h-full object-cover md:rounded-2xl md:order-1"
-          src="illustration-sign-up-mobile.svg"
-        />
+    <main className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full sm:max-w-[375px] md:max-w-[950px] 2xl:scale-125 shadow-2xl">
+      <article className="bg-white sm:rounded-2xl md:rounded-3xl overflow-hidden min-h-screen sm:min-h-min grid md:grid-cols-2 md:p-8 md:gap-4">
+        <picture>
+          <source
+            media="(min-width: 768px)"
+            srcSet="illustration-sign-up-desktop.svg"
+            className="md:rounded-2xl md:order-1"
+          />
+          <img
+            className="w-full"
+            src="illustration-sign-up-mobile.svg"
+            alt="background"
+          />
+        </picture>
         <section className="px-8 text-slate md:px-10">
           <h1 className="text-[2.5rem] md:text-[2.8rem] lg:text-[3.4rem] font-bold mt-6">
             Stay Updated!
