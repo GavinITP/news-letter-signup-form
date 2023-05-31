b69420fb788748fa25569a95717cc621cf6f5211
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-05-31 12:23:06 +0700

    responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 const Home = () => {
   return (
-    <main className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full max-w-[375px]">
-      <article className="bg-white rounded-2xl overflow-hidden max-h-screen">
-        <img className="w-full" src="illustration-sign-up-mobile.svg" />
-        <section className="px-8 text-slate">
-          <h1 className="text-[2.5rem] font-bold mt-6">Stay Updated!</h1>
+    <main className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full max-w-[375px] md:max-w-[1000px] 2xl:scale-125 shadow-2xl">
+      <article className="bg-white rounded-2xl md:rounded-3xl overflow-hidden max-h-screen grid md:grid-cols-2 md:p-8 md:gap-8">
+        <img
+          className="w-full md:h-full object-cover md:rounded-2xl md:order-1"
+          src="illustration-sign-up-mobile.svg"
+        />
+        <section className="px-8 text-slate md:px-10">
+          <h1 className="text-[2.5rem] md:text-[2.8rem] lg:text-[3.4rem] font-bold mt-6">
+            Stay Updated!
+          </h1>
           <p className="my-4 text-base">
             Join 60,000+ product managers receiving monthly updates on:
           </p>
